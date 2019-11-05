7f76a76478106e93813645e0a0ce048afe274242
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-11-05 16:00:09 +0700

    Initial Commit

---
+++ HangmanGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HangmanGame
+//
+//  Created by alexander on 05.11.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
